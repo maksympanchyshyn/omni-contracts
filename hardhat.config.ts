3697fb799a26f332355627252e4eefca1ba03979
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2024-01-06 17:46:53 +0200

    Add hardhat and env configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-solhint';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.20',
+  defaultNetwork: 'hardhat',
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+    arbitrumOne: {
+      chainId: 42161,
+      url: process.env.ARBITRUM_ONE_URL || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY || '',
+      arbitrumOne: process.env.ARBISCAN_API_KEY || '',
+      optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY || '',
+    },
+  },
+};
+
+export default config;
