8dd3a14842fba99ddf82c99836eba581e76dee65
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2024-01-22 17:22:37 +0200

    Add GasStation contract for gas refuel

---
+++ contracts/GasStation.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import '@layerzerolabs/solidity-examples/contracts/lzApp/NonblockingLzApp.sol';
+
+/**
+ * @author Maksym Panchyshyn
+ * @title GasStation
+ */
+contract GasStation is NonblockingLzApp {
+  using BytesLib for bytes;
+
+  constructor(address _lzEndpoint) NonblockingLzApp(_lzEndpoint) {}
+
+  function estimateSendFee(
+    uint16 _dstChainId,
+    bytes memory payload,
+    bytes memory _adapterParams
+  ) public view virtual returns (uint nativeFee, uint zroFee) {
+    return lzEndpoint.estimateFees(_dstChainId, address(this), payload, false, _adapterParams);
+  }
+
+  function refuelGas(
+    uint16 _dstChainId,
+    bytes memory _toAddress,
+    bytes memory _adapterParams
+  ) public payable virtual {
+    _checkGasLimit(_dstChainId, 0, _adapterParams, 0);
+    (uint nativeFee, ) = estimateSendFee(_dstChainId, _toAddress, _adapterParams);
+    require(msg.value >= nativeFee, 'Not enough gas to send');
+
+    _lzSend(_dstChainId, _toAddress, payable(0x0), address(0x0), _adapterParams, nativeFee);
+  }
+
+  function withdraw() public payable onlyOwner {
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}('');
+    require(success);
+  }
+
+  function _nonblockingLzReceive(
+    uint16 _srcChainId,
+    bytes memory _srcAddress,
+    uint64 _nonce,
+    bytes memory _payload
+  ) internal virtual override {}
+}
